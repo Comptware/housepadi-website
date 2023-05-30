d647abe720bc0b8a555a1302a5bdda1119d47a50
jibola <ajibolaajiboye99@gmail.com>
2023-05-30 17:12:12 +0100

    dev updates

--- src/pages/landingWeb/SectionFour.js
+++ src/pages/landingWeb/SectionFour.js
@@ -4,7 +4,28 @@ import Chevron_down from '../../assets/icons/chevron_down.svg'
 
 const SectionFour = () => {
 
-    // const [dropDown, setdropDown] = useState(true)
+    const [open, setOpen] = useState(true);
+    const [open2, setOpen2] = useState(true);
+    const [open3, setOpen3] = useState(true);
+    const [open4, setOpen4] = useState(true);
+
+    const handleOpen = () => {
+        setOpen(!open);
+    };
+
+    const handleOpen2 = () => {
+        setOpen2(!open2);
+    };
+
+    const handleOpen3 = () => {
+        setOpen3(!open3);
+    };
+
+    const handleOpen4 = () => {
+        setOpen4(!open4);
+    };
+    
+
 
     return (
         <div className='pt-[100px] pb-[140px] bg-[#F6F6F6] px-[20px] sm:px-[50px] md:px-[70px] slg:px-[100px] text-[#002616]'>
@@ -17,18 +38,21 @@ const SectionFour = () => {
                     <div className='flex justify-between'>
                         <p className='font-bold'>Can I choose my meals?</p>
 
-                        <Chevron_down />
+                        <button onClick={handleOpen}>{open ? <Chevron/> : <Chevron_down/>}</button>
                     </div>
 
-                    <p className='text-[#3c3c43b5]'>Quisque rutrum. Aenean imperdi. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget.</p>
+                    {open ? '' : <div><p className='text-[#3c3c43b5]'>Quisque rutrum. Aenean imperdi. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget.</p></div>}
+
                 </div>
 
                 <div className='bg-white p-[30px] rounded-[12px] text-[18px] flex flex-col gap-[26px]'>
                     <div className='flex justify-between'>
                         <p className='font-bold'>When will i recieve my order?</p>
 
-                        <Chevron />
+                        <button onClick={handleOpen2}>{open2 ? <Chevron/> : <Chevron_down/>}</button>
                     </div>
+
+                    {open2 ? '' : <div><p className='text-[#3c3c43b5]'>Lorem ipsum. Aenean imperdi. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget.</p></div>}
 
                 </div>
 
@@ -36,8 +60,10 @@ const SectionFour = () => {
                     <div className='flex justify-between'>
                         <p className='font-bold'>Can I skip a delivery?</p>
 
-                        <Chevron />
+                        <button onClick={handleOpen3}>{open3 ? <Chevron/> : <Chevron_down/>}</button>
                     </div>
+
+                    {open3 ? '' : <div><p className='text-[#3c3c43b5]'>propanga ipsum. Aenean imperdi. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget.</p></div>}
 
                 </div>
 
@@ -45,10 +71,13 @@ const SectionFour = () => {
                     <div className='flex justify-between'>
                         <p className='font-bold'>Can I add an Extra to my delivery?</p>
 
-                        <Chevron />
+                        <button onClick={handleOpen4}>{open4 ? <Chevron/> : <Chevron_down/>}</button>
                     </div>
 
+                    {open4 ? '' : <div><p className='text-[#3c3c43b5]'>Etiam ultricies nisi. Aenean imperdi. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget.</p></div>}
+
                 </div>
+
             </div>
 
 
