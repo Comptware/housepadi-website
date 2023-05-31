68406bcc806d8270276d50bf42a9738f6575e93a
jibola <ajibolaajiboye99@gmail.com>
2023-05-31 23:45:38 +0100

    frontpage slide

---
+++ src/utils/greetings.js
@@ -0,0 +1,14 @@
+export const GREETINGS = [
+    {brand: 'Laundry Service', content: 'Get clean clothes within 48 hours, FREE pick up and delivery'},
+    {brand: ' Chef Cooked meals', content: 'Get delicious meals delivered to you on a schedule' },
+    {brand: ' Cleaning Services', content: 'Relax, while your home cleaning needs are taken care of by trained professionals' },
+    {brand: ' Grocery shopping', content: 'Let us ease your stress of grocery shopping on the weekend by delivering your basic needs while offering unbeatable prices' }
+  ];
+
+
+// export const GREETINGS = [
+//     'Laundry Service',
+//      ' Chef Cooked meals',
+//      ' Cleaning Services',
+//      ' Grocery shopping'
+// ];
